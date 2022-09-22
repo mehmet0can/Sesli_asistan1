1467dd99a6be208ed757bba23b5b9b40ae808e87
mehmet0can <mehmetcanatar96@gmail.com>
2022-09-22 19:54:10 +0300

    Update sesli.py

--- sesli.py
+++ sesli.py
@@ -6,6 +6,8 @@ from optparse import OptionParser
 
 try:
     Parser = OptionParser()
+    # Bu kısımda 'Parser' olşturarak bu parserın option özelliklerini kullanıyorum
+    # bu sayede parametreler oluşturup kullanabiliyorum
     Parser.add_option("-d", dest="data", help="mesajı parametre olarak giriniz")
     Parser.add_option("-n", dest="name", help="mp3 dosyasının ismi")
     (user_input, arguments) = Parser.parse_args()
@@ -20,12 +22,16 @@ except KeyboardInterrupt:
 except Exception as E:
     print()
 
+# Eğer ki işletim sistemi windows ise Documents klasörüne giderek bir ses klasörü
+# oluşturuyorum ve tüm eklemeri orada gerçekleştiriyorum.
 if os.name == 'nt':
     subprocess.call(["cd Documents"])
     subprocess.call(["mkdir voice_file"])
     speech = gTTS(text=str(data), lang='tr')
     speech.save(name + ".mp3")
 
+# Eğer ki işletim sistemi Linux yada MAC OS ise Documents klasörüne giderek bir ses klasörü
+    # oluşturuyorum ve tüm eklemeri orada gerçekleştiriyorum.
 elif os.name == "posix":
     subprocess.call(["cd Documents"])
     subprocess.call(["mkdir voice_file"])
